3a8dd910a6bb24c29f87a18798a4f3a97221bd26
Maria Teaca <maria.teaca@stud.acs.upb.ro>
2024-03-09 14:58:44 +0200

    base folders and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Landing from "./components/landing/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter className="main-container">
+      {/* <NavBar/> */}
+      <div className="dashboard container-fluid">
+        <div className="row">
+          <Routes>
+            <Route path="/" exact element={<Landing />} />
+            <Route
+              path="*"
+              element={<Navigate to="/" />}
+            />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
