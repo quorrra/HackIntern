c2a757fedd4fd70bf236b4b5ddcbeb7e8011d657
Andreea Povarna <93869336+povi24@users.noreply.github.com>
2024-03-09 16:55:28 +0200

    landing page merge mai frumos facuta

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -4,6 +4,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import React from 'react';
+//import image froma ssets
+import Image from '../../assets/landingpoza.svg';
+
+import './LandingStyle.css';
 
 function Landing() {
   return (
@@ -51,6 +55,21 @@ function Landing() {
 
 
       ))}
+      <Container>
+        <div className="d-flex justify-content-center gap-5">
+            <div className="ms-6"> 
+                <img  className="pt-5"src={Image} alt="LandingImage" />
+            </div>
+            <div className="m-5 pb-5 ">
+                <h1>Learn-IT-all</h1>
+                <p>În lumea magică a Learn-It-All, copiii devin eroi ai cunoașterii! Aici, ei își creează propriul lor personaj și își cresc puterile (adică cunoștințele) rezolvând teste distractive și jucându-se. Cu cât trec mai multe teste, cu atât devin mai puternici și deblochează cariere grozave! De la explorarea matematicii până la aventurile în lumea cărților, Learn-It-All îi ajută pe copii să-și urmeze visurile și să devină mari!</p>
+            </div>
+            
+        </div>
+</Container>
+
+
+
     </>
   );
 }
